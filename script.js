9b6ca70debd22bade582dce12e8d7677cb9954fc
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-02-12 19:27:15 +0530

    Update

--- script.js
+++ script.js
@@ -15,10 +15,14 @@ countries.forEach((country) => {
   const option2 = document.createElement("option");
 
   option1.value = option2.value = country.code;
-  option1.textContent = option2.textContent = `${country.code} (${country.name})`;
+  option1.textContent =
+    option2.textContent = `${country.code} (${country.name})`;
 
   fromCurrencyElement.appendChild(option1);
   toCurrencyElement.appendChild(option2);
+
+  fromCurrencyElement.value = "USD";
+  toCurrencyElement.value = "INR";
 
   //can also write this abv code
   //   const option1 = document.createElement("option");
@@ -32,4 +36,22 @@ countries.forEach((country) => {
   //   toCurrencyElement.appendChild(option2);
 });
 
-//funct to get exchnage reate using tag
+//funct to get exchnage rate using tag
+const getExchangeRate = async () => {
+  const amount = parseFloat(fromAmountElement.value);
+  const fromCurrency = fromCurrencyElement.value;
+  const toCurrency = toCurrencyElement.value;
+
+  //fetch data from api
+  const response = await fetch(
+    `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`,
+  );
+  const data =await response.json();
+
+  const conversionRate = data.rates(toCurrency);
+  const convertedAmount = (amount * conversionRate);
+
+  convertedAmountElement.value = convertedAmount;
+};
+
+fromAmountElement.addEventListener("input", getExchangeRate);
